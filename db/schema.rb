f06addfe309a2d5194a0d53272274eafc2fab3cf
combatcellist <combatcellist913@gmail.com>
2021-01-21 00:23:51 +0900

    ログイン、ログアウト機能実装

--- db/schema.rb
+++ db/schema.rb
@@ -44,8 +44,6 @@ ActiveRecord::Schema.define(version: 2021_01_20_103254) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "name", null: false
-    t.string "detail", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
